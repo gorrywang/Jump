f542ac8aa35dd6b3b395e6dadc9164ac947cd15c
gorrywang <isgorry@gmail.com>
2017-08-16 21:01:43 +0800

    Project init.

---
+++ app/src/main/java/com/github/www/jump/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.www.jump.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.www.jump.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
